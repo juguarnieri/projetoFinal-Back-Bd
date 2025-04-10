2deabeaa38106ba5a6ab29a5221d626925bc4795
LuizGabrielLopes <luiz.gabriellopesc@gmail.com>
2025-04-10 15:48:01 -0300

    feat: fix report Routes

--- src/Routes/reportRoutes.js
+++ src/Routes/reportRoutes.js
@@ -3,9 +3,6 @@ const router = express.Router();
 const reportController = require("../controllers/reportController");
 
 router.get("/reports/users/pdf", reportController.exportUsersPDF);
-router.get("/reports/users/pdf", reportController.exportUsersPDF);
-router.get("/reports/posts/pdf", reportController.exportPostsPDF);
-router.get("/reports/users/pdf", reportController.exportUsersPDF);
 router.get("/reports/posts/pdf", reportController.exportPostsPDF);
 router.get("/reports/comments/pdf", reportController.exportCommentsPDF);
 router.get("/reports/news/pdf", reportController.exportNewsPDF);
