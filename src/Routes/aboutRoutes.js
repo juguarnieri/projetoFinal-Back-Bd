e92b0c8d7badea3dbb0e8f88f24c87f2001cf9b2
LuizGabrielLopes <luiz.gabriellopesc@gmail.com>
2025-04-24 16:27:37 -0300

    feat: about swagger

--- src/Routes/aboutRoutes.js
+++ src/Routes/aboutRoutes.js
@@ -10,7 +10,6 @@ router.get("/", aboutController.getAboutPage);
  *   name: About
  *   description: Nossa página Sobre nós
  */
-
 /**
  * @swagger
  * /api/about:
@@ -31,11 +30,123 @@ router.get("/team", aboutController.getAllMembers);
 
 
 router.post("/", aboutController.createAbout);
+
+
+
 router.put("/:id", aboutController.updateAbout);
-router.get("/team", aboutController.getAllMembers);   
+
+/**
+ * @swagger
+ * /api/about/team:
+ *   post:
+ *     summary: Cria um novo membro
+ *     tags: [About]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               role:
+ *                 type: string
+ *               main_title:
+ *                 type: string
+ *               subtitle:
+ *                 type: string
+ *               description:
+ *                 type: string
+ *               commitment_title:
+ *                 type: string
+ *               commitment_text:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: Membro criado
+ */
+
 router.post("/team", aboutController.addMember);
+
+/**
+ * @swagger
+ * /api/about/{id}:
+ *   put:
+ *     summary: Atualiza um membro
+ *     tags: [About]
+ *     parameters:  
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               role:
+ *                 type: string
+ *               main_title:
+ *                 type: string
+ *               subtitle:
+ *                 type: string
+ *               description:
+ *                 type: string
+ *               commitment_title:
+ *                 type: string
+ *               commitment_text:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Casa atualizada
+ */
+
+
 router.put("/team/:id", aboutController.updateMember);
+/**
+ * @swagger
+ * /api/about/team/{id}:
+ *   get:
+ *     summary: Busca os membros da equipe por ID
+ *     tags: [About]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Membro encontrado
+ *       404:
+ *         description: Membro não encontrado
+ */
 router.get("/team/:id", aboutController.getMemberById);
+
+/**
+ * @swagger
+ * /api/about/team/{id}:
+ *   delete:
+ *     summary: Deleta um membro
+ *     tags: [About]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Membro deletado
+ */
+
+
 router.delete("/team/:id", aboutController.deleteMember);
 
 module.exports = router;
