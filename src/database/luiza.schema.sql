d3d5ba5f7f03718e5e5a661f3b2d2ca2d635f7f4
luschettini <luizaschettinii@gmail.com>
2025-05-08 15:23:56 -0300

    feat: add an insert to the news table

---
+++ src/database/luiza.schema.sql
@@ -0,0 +1,19 @@
+-- INSERTS de notícias pesquisadas
+
+
+INSERT INTO news (title, description, link, image, category, decade, year, is_featured)
+VALUES 
+
+('Assassinato de Liana Friedenbach e Felipe Caffé
+', 'O assassinato de Liana Friedenbach e Felipe Caffé em novembro de 2003 foi um dos crimes mais chocantes da história brasileira, mobilizando a opinião pública e reacendendo debates sobre segurança e legislação penal. O casal, que namorava há cerca de dois meses, decidiu viajar para acampar na zona rural de Embu-Guaçu, São Paulo. Para isso, Liana mentiu para os pais, dizendo que participaria de uma excursão para Ilhabela, enquanto Felipe omitiu que estaria apenas com ela. No dia 31 de outubro, partiram para o sítio abandonado onde montaram acampamento.
+
+No dia seguinte, ao caminharem pela região, cruzaram com Roberto Aparecido Alves Cardoso, conhecido como "Champinha", de 16 anos, e Paulo César da Silva Marques, o "Pernambuco", de 32 anos. Os criminosos perceberam que o casal estava desprotegido e decidiram abordá-los. Na tarde de 1º de novembro, invadiram a barraca, armados, e os levaram para uma área isolada na mata. Mantidos em cativeiro, Felipe foi assassinado na manhã do dia 2 de novembro com um tiro na nuca por Pernambuco, enquanto Liana foi violentada repetidamente por Champinha e outros cúmplices por dias, antes de ser brutalmente morta.
+
+O desaparecimento dos jovens mobilizou familiares e autoridades. O pai de Liana, Ari Friedenbach, intensificou as buscas com helicópteros e panfletagem. A polícia identificou Champinha como suspeito e, após sua captura, ele revelou os locais onde os corpos estavam. Felipe havia sido executado com um único tiro, enquanto Liana apresentava múltiplos ferimentos por arma branca.
+
+Os criminosos foram julgados e condenados. Pernambuco recebeu pena de 110 anos de prisão, enquanto Antônio Caetano da Silva foi sentenciado a 124 anos. Agnaldo Pires recebeu 47 anos, e Antônio Matias de Barros foi condenado a seis anos. Champinha, por ser menor de idade, cumpriu três anos na Fundação CASA e, devido a avaliações psiquiátricas que indicaram transtorno de personalidade antissocial, foi transferido para uma unidade psiquiátrica onde permanece até hoje.
+
+O caso gerou forte impacto social e reacendeu debates sobre a maioridade penal no Brasil. Ari Friedenbach tornou-se um defensor de mudanças na legislação para endurecer penas e garantir maior proteção aos jovens. A tragédia evidenciou fragilidades do sistema de segurança e justiça, impulsionando discussões sobre reforma penal e medidas preventivas contra crimes violentos.
+​', 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fveja.abril.com.br%2Fcoluna%2Fradar%2Fos-motivos-de-friedenbach%2F&psig=AOvVaw3TqFflUPctiweo8Yt4wOJz&ust=1746812657040000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCIiBvMy2lI0DFQAAAAAdAAAAABAE', 'Homicídio-qualificado', 'anos 2000', 2000, TRUE),
+
+
